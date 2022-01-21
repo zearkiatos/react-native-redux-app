e391b37242ba5c2b82583795f67163741762db2e
zearkiatos <caprilespe@outlook.com>
2022-01-21 15:04:42 -0300

    Creating our first application with redux

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStore, combineReducers } from 'redux';
+import { Provider } from "react-redux";
+import { StyleSheet, Text, View } from "react-native";
+
+const reducers = {
+  todos: (state = [], action) => {
+    console.log(action);
+    return state;
+  }
+};
+
+const AppUi = () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+  </View>
+);
+
+const store = createStore(combineReducers({
+  ...reducers,
+}))
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+        <AppUi />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ README.md
@@ -0,0 +1,2 @@
+## Description
+This is a react native ⚛️ 📱 application with a redux implementation to practice how react native work with redux.
