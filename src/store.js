9390091a5f6834e333bd9b4d9d6743b337942606
zearkiatos <caprilespe@outlook.com>
2022-01-24 20:53:18 -0300

    Intro to redux thunk

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import reducers from "./reducers";
 const store = createStore(
   combineReducers({
     ...reducers
-  })
+  }),
+  applyMiddleware(thunk)
 );
 export default store;
