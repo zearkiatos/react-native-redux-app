37449ef7021de97190a941eb12fab8d2b118e2d2
zearkiatos <caprilespe@outlook.com>
2022-01-26 19:34:44 -0300

    Creating our first thunk

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,4 +1,5 @@
 import { TODO_TYPES } from "../types";
+import config from '../config';
 
 const submit = (text) => ({
   type: TODO_TYPES.SUBMIT,
@@ -13,5 +14,26 @@ const complete = (id) => ({
   type: TODO_TYPES.COMPLETE,
   payload: id
 });
+
+const startSubmit = () => ({
+  type: TODO_TYPES.START_SUBMIT
+});
+
+const errorSubmit = (error) => ({
+  type: TODO_TYPES.ERROR_SUBMIT,
+  error
+});
+
+const saveTodo = (text) => async (dispatch, getState) => {
+  const state = getState();
+  console.log(state);
+  dispatch(startSubmit());
+  try {
+    const response = await fetch(`${config.JSON_PLACEHOLDER_API_BASE_URL}/todos`);
+    dispatch(submit(text));
+  } catch (ex) {
+    dispatch(errorSubmit(ex));
+  }
+};
 
 export default { submit, complete };
